1f55b1f36ad8c5dd93bff0a982e39a09edccb270
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-12-25 16:06:27 +0900

    [style]: 글로벌 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import styled from 'styled-components';
-import Header from './components/common/Header';
+import Header from './components/commons/Header';
+import Main from './components/main/Main';
 
 function App() {
   return (
     <Home>
       <Header />
+      <Main></Main>
     </Home>
   );
 }
 
 const Home = styled.div`
   width: 100vw;
+
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 export default App;
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,12 @@
 import { reset } from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`${reset}`;
+const GlobalStyle = createGlobalStyle`${reset}
+    button {
+        outline: none;
+        border: 0;
+        cursor: pointer;
+    }
+`;
 
 export default GlobalStyle;
