eb5b460098b013f637c072bb9243764911fe8d65
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-12-30 16:31:53 +0900

    [style]: tweet section 추가

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
 import Overview from './Overview/Overview';
+import Tweet from './Tweet/Tweet';
 
 function Main() {
   return (
     <MainContainer>
       <Overview></Overview>
+      <Tweet></Tweet>
     </MainContainer>
   );
 }
 
 const MainContainer = styled.div`
   width: 100%;
-  max-width: 1440px;
-  padding: 120px 0;
+  max-width: 1360px;
+  padding: 120px 80px;
 
   display: flex;
   flex-direction: column;
